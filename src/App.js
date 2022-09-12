9642940bfff9ab8863e02e5a756e6622f3039afc
sebastian ortiz <sebastianleonel97@hotmail.com>
2022-09-12 19:37:37 -0300

    subo proyecto

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react" 
 import ItemListContainer from "./components/ItemListContainer.js"
 import Navbar from "./components/Navbar"
+import Count from "./components/itemCount"
 
 
 const App =()=>{
     const bienvenida = "Saturno, donde la tierra y el alma se conecta ";
+    const onADD =(count)=>{
+        console.log(`compraste ${count} unidades `);
+    }
 
 
     return (
         <>
         <Navbar />
         <ItemListContainer greeting={bienvenida}  />
+        <Count stock={10} initial={1} onADD={onADD}/>
         </>
     )
         
---
+++ src/components/itemCount.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import collar from "../img/collar.jpg"
+
+
+
+
+function ItemCount({ stock, initial, onADD}) {
+    const [count, setCount] = useState(1);
+
+    const sumar = () => {
+        setCount(count + 1)
+    }
+
+    const restar = () => {
+        setCount(count - 1)
+    }
+
+    const reset = () => {
+        setCount(0)
+    }
+
+    
+
+    return (
+        <main>
+
+        <div className='ContenedorCard'>
+            <img className='imgProd' src={collar} alt="" />
+            <h3>Collar <span>ONIX NEGRO</span></h3>
+            <h4>{count}</h4>
+            <div className='ContenedorContador'>
+                <button disabled={count >= stock} onClick={sumar} className='sumar'><i class="fa-solid fa-plus"></i></button>
+                <button disabled={count <= 1} onClick={restar} className='rest'><i class="fa-solid fa-minus"></i></button>
+                <button onClick={reset} className='reset'><i class="fa-solid fa-rotate-right"></i></button>
+                <button disabled={stock<=0} onClick={()=> onADD(count)}><i class="fa-sharp fa-solid fa-cart-shopping"></i></button>
+            </div>
+
+        </div>
+        </main>
+    )
+}
+
+export default ItemCount
+
